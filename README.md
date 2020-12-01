7cb755f01c9183699f0920f2daa6e5d2c01aec9c
Soumen Khara <soumen2015.s.k@gmail.com>
2020-12-01 20:39:13 +0530

    Complete the first version

---
+++ README.md
@@ -0,0 +1,3 @@
+# GEN IGNORE
+
+Simple command line tool to generate gitignore file for specific language
---
+++ index.js
@@ -0,0 +1,95 @@
+#!/usr/bin/env node
+const fs = require("fs");
+const ignores = process.argv[1].split("index.js")[0] + "ignores";
+const usage = `┏━╸┏━╸┏┓╻   ╻┏━╸┏┓╻┏━┓┏━┓┏━╸
+┃╺┓┣╸ ┃┗┫   ┃┃╺┓┃┗┫┃ ┃┣┳┛┣╸
+┗━┛┗━╸╹ ╹   ╹┗━┛╹ ╹┗━┛╹┗╸┗━╸
+gen-ignore is a command line tool that generate gitignore file for specific programming language
+
+Usage gen-ignore [option]
+      gen-ignore [programing language]
+
+Options:
+ -v or --version                    version of the tool
+ -h or --help                       help menu 
+
+Documentation can be found in https://50um3n.github.io/gen-ignore`;
+
+const ignoreFileName = ".gitignore";
+
+function createFile(location) {
+  fs.writeFile(location, "", (err, data) => {
+    console.log("File is created");
+  });
+}
+
+function checkExistence() {
+  fs.access(ignoreFileName, (err) => {
+    if (err) {
+      console.log("File is not exist!!! Creating the file");
+      createFile(ignoreFileName);
+    } else {
+      console.log("File is already exist");
+      fs.rename(ignoreFileName, ".gitignore.bk", (err, data) => {
+        console.log("Existing file is renamed to .gitignore.bk");
+        createFile(ignoreFileName);
+      });
+    }
+  });
+}
+
+switch (process.argv[2]) {
+  case "node":
+    checkExistence();
+    genIgnore("Node.gitignore");
+    break;
+  case "c":
+    checkExistence();
+    genIgnore("C.gitignore");
+    break;
+  case "cpp":
+    checkExistence();
+    genIgnore("C++.gitignore");
+    break;
+  case "java":
+    checkExistence();
+    genIgnore("Java.gitignore");
+    break;
+  case "python":
+    checkExistence();
+    genIgnore("Python.gitignore");
+    break;
+  case "vs":
+    checkExistence();
+    genIgnore("VisualStudio.gitignore");
+    break;
+  case "ruby":
+    checkExistence();
+    genIgnore("Ruby.gitignore");
+    break;
+  case "Kotlin":
+    checkExistence();
+    genIgnore("Kotlin.gitignore");
+    break;
+  case "--version":
+  case "-v":
+    const version = require("./package.json");
+    console.log("v" + version.version);
+    break;
+  case "--help":
+  case "-h":
+    console.log(usage);
+    break;
+  default:
+    console.log(usage);
+}
+
+function genIgnore(fname) {
+  fs.readFile(`${ignores}/${fname}`, "utf8", (err, data) => {
+    fs.appendFile(ignoreFileName, data, (err, da) => {
+      if (da) {
+        console.log("File created");
+      }
+    });
+  });
+}
